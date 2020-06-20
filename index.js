b6df3e0dc3b65288be92f018130d5357dd34f4d0
Mohamed Sheikh <mohamed-sheikh1@outlook.com>
2020-06-20 22:03:49 +0100

    Complete

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function handler() {
             reject(err);
           }
           resolve(JSON.parse(data));
+          console.log("retrieved environment variables");
           process.env = JSON.parse(data);
         });
       };
@@ -27,6 +28,7 @@ async function handler() {
     let result = (resolve, reject) => {
       try {
         client = new twilio(process.env.accountSid, process.env.authToken);
+        console.log("Initialised Twilio");
         resolve(client);
       } catch (error) {
         console.log(error);
@@ -41,13 +43,13 @@ async function handler() {
       try {
         client.messages
           .create({
-            body: `${e.content} is overdue`,
+            body: `${e.content} is overdue. Should have been done by ${e.due.date}`,
             to: process.env.MyNumber, // Text this number
             from: process.env.twilioPhone, // From a valid Twilio number
           })
           .then((message) => {
             console.log(message.sid);
-            console.log("sent");
+            console.log("sent message");
             resolve(message.sid);
           });
       } catch (error) {
@@ -71,6 +73,7 @@ async function handler() {
           response.data.map((item) => {
             projectIDS.push(item.id);
           });
+          console.log("retrieved all projects");
           resolve(projectIDS);
         });
       } catch (error) {
@@ -81,7 +84,7 @@ async function handler() {
     return new Promise(result);
   }
 
-  async function getAllActiveTasks() {
+  async function getAllOverdueTasks() {
     const url = "https://api.todoist.com/rest/v1/tasks";
 
     try {
@@ -93,26 +96,68 @@ async function handler() {
             headers: {
               Authorization: `Bearer ${process.env.token}`,
             },
-          }).then((response) => {
-            overdueProjects.push(response.data);
+          }).then(async (response) => {
+            response.data.map(async (i) => {
+              if (
+                i.due &&
+                moment().subtract(1, "day").isAfter(moment(i.due.date))
+              ) {
+                await overdueProjects.push(i);
+              }
+            });
           });
         })
       );
     } catch (error) {
       console.log(error);
     }
+    console.log("retrieved all overdue projects");
     return overdueProjects;
+  }
+
+  async function determineNextStep() {
+    var tasksToDo = "";
+    try {
+      overdueProjects.map(async (element, index) => {
+        if (element.due) {
+          if (moment().diff(moment(element.due.date), "day") > 2) {
+            tasksToDo +=
+              " Task " + (index + 1).toString() + " " + element.content;
+          } else {
+            await sendSMS(element);
+          }
+        }
+      });
+      console.log(tasksToDo);
+      if (tasksToDo.length > 1) {
+        await makeCall(tasksToDo);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function makeCall(tasks) {
+    try {
+      client.calls
+        .create({
+          twiml: `<Response><Say>${tasks}</Say></Response>`,
+          to: process.env.MyNumber,
+          from: process.env.twilioPhone,
+        })
+        .then((call) => console.log(call.sid));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   await getEnvironment();
   await initialiseTwilio();
-  let a = await getAllProjects();
-  getAllActiveTasks();
-
-  //   overdue.map((e) => {
-  //     sendSMS(e);
-  //   });
-  // }
+  await getAllProjects();
+  let overdue = await getAllOverdueTasks();
+  if ((await overdue.length) > 0) {
+    await determineNextStep();
+  }
 }
 
 handler();
